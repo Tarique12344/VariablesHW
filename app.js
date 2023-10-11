b7c855d3e14e1e545bed8f884e672f8cdb894721
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-10-11 17:03:10 -0400

    VariablesHW

---
+++ app.js
@@ -0,0 +1,28 @@
+var a = 10
+var b = 5
+var c = 15
+var d = 20
+var e = 25
+var f = 30
+var h = 35
+var i = 40
+var j = 45
+var k = 50
+
+console.log(a + b);
+console.log(e + d);
+
+console.log(k - e);
+console.log(f - a);
+
+console.log(k * i);
+console.log(j * b);
+
+console.log(k / b);
+console.log(j / h);
+
+console.log(++a);
+console.log(++c);
+
+console.log(--j)
+console.log(--k)
